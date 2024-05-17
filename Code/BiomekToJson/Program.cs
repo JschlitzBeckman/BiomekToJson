db84f5f6c310af990ac56e0f413428fe5cbbd209
Jacob Schlitz <jschlitz@beckman.com>
2024-05-17 13:58:32 -0400

    interop

--- Code/BiomekToJson/Program.cs
+++ Code/BiomekToJson/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OthrosNet;
 
 namespace BiomekToJson
 {
@@ -10,6 +12,20 @@ namespace BiomekToJson
   {
     static void Main(string[] args)
     {
+      var test = new Eeor();
+      test.Put("Hello", "World!");
+      test.Put("int", 413);
+      test.Put("double", 612.0);
+      test.Put("array", new[] { 1, 2, 3, 4, 5 });
+      test.Put("object", new Eeor ());
+      var vl = new VariantList { "Foo", "Bar", "Baz" };
+      test.Put("List", vl);
+
+      var simple = JsonConvert.SerializeObject(test, Formatting.Indented);
+
+      Console.WriteLine(simple);
+
+      Console.ReadKey(true);
     }
   }
 }
